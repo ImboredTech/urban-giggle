e8b8f1f8a912e2a14e66ec79cdc2353cb786126d
ImboredTech <106279751+ImboredTech@users.noreply.github.com>
2022-08-24 16:59:52 +0100

    Update bot.py, loads cogs on startup. Created commands to load unload and reload cogs inside discord.

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import config
-
+import os
 
 bot = commands.Bot(command_prefix="!", owner_id=config.owner_id)
 
@@ -13,6 +13,43 @@ async def on_ready():
     print("Bot is online.")
     await bot.change_presence(activity=discord.Game(name="!help for commands."))
 
+
+# Loads a cog.
+@bot.command()
+async def load(ctx, extension):
+    if ctx.author.id == config.owner_id:
+        bot.load_extension(f"cogs.{extension}")
+        await ctx.send(f"Loaded {extension}.")
+    else:
+        await ctx.send("You do not have permission to use this command.")
+
+
+# Unloads a cog.
+@bot.command()
+async def unload(ctx, extension):
+    if ctx.author.id == config.owner_id:
+        bot.unload_extension(f"cogs.{extension}")
+        await ctx.send(f"Unloaded {extension}.")
+    else:
+        await ctx.send("You do not have permission to use this command.")
+
+
+# Reloads a cog
+@bot.command()
+async def reload(ctx, extension):
+    if ctx.author.id == config.owner_id:
+        bot.reload_extension(f"cogs.{extension}")
+        await ctx.send(f"Reloaded {extension}.")
+    else:
+        await ctx.send("You do not have permission to use this command.")
+
+
+# Loads all cogs on startup
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+    else:
+        print(f"Failed to load {filename}")
 
 
 bot.run(config.token)
